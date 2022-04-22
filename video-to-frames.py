f15e38f04071b4101328f78e7aa896d7ac4742eb
TurboSjaantje <annedvdmeulen@gmail.com>
2022-04-22 13:11:17 +0200

    video to images

--- video-to-frames.py
+++ video-to-frames.py
@@ -1,1 +1,20 @@
-print ('Hello world')
+import cv2
+import os
+
+# Opens the Video file
+cap = cv2.VideoCapture('.\\parts\\video\\example.mp4')
+i = 0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite(os.path.join('./parts/frames','frame'+str(i)+'.jpg'), frame)
+    i += 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+print('finished with splitting video into images!')
+
+#-------------------------------------------------------------------
+
